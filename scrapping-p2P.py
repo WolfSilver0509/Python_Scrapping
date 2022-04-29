d4d3473fb4fbfef219ce47ccc2617b1741201daf
Denizot <denizot.myriam@gmail.com>
2022-04-29 07:15:35 +0200

    bases

---
+++ scrapping-p2P.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+url = "https://books.toscrape.com/index.html"
+page = requests.get(url)
+# Voir le code html source
+soup = BeautifulSoup(page.content, 'html.parser')
+#cat = soup.find_all("div", class_="side_categories")
+#cat = soup.find("div", { "class" : "side_categories" }).findAll("a", recursive=False)
+#categories = cat.findChildren("a")
+#for div in cat:
+
+div = soup.find("div", { "class" : "side_categories" })
+cat = div.find_all("a") # returns a list of all <a> children of li
+
+print(cat)
